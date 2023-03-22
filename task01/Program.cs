ecb0781c1e233723de117598474e7b5ac1eaa9d7
Andranik <andran1401@gmail.com>
2023-03-23 00:16:41 +0400

    задача41

---
+++ task01/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 1: Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+int Counting()
+{
+    int count = 0;
+    string word;
+
+    while (true)
+    {
+        Console.Write("введите число: ");
+        word = Console.ReadLine()!;
+
+        if (word == "") return count;
+        else if (int.Parse(word) > 0) count ++;
+        
+        
+
+
+    }
+}
+
+int count = Counting();
+Console.WriteLine($"больше нуля: {count}");
