01fa27774dd61e4d4a729e4ee3771a7b13d81217
Andranik <andran1401@gmail.com>
2023-03-23 14:05:21 +0400

    задача43

---
+++ task02/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+void Intersection(double k1, double b1, double k2, double b2)
+{
+    double k_sub = k1 - k2;
+    if (k_sub != 0)
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Section ({x}; {y})");
+
+
+    }
+    else if (k1 == k2 && b1 == b2)
+        Console.WriteLine("here it is ");
+    else
+        Console.WriteLine("no sections");
+
+}
+
+
+
+
+
+
+
+
